22ae7fe222faab2ccae5120dcf43f8f214a7fcdc
Jap Grover <japgrover@macbookair.home>
2026-07-15 13:55:44 -0400

    fix: keep EPUB books catalog driven

--- src/pages/Banis.tsx
+++ src/pages/Banis.tsx
@@ -35,6 +35,7 @@ import { getSearchModeLabels } from '../utils/translations'
 import { IconArrowRight, IconSearch, IconChevronUp, IconChevronDown, IconLibrary, IconBookmark, IconBookmarkFilled, IconMusic } from '../components/icons'
 import SearchHighlight from '../components/SearchHighlight'
 import ScriptureSourceBrowser from '../components/ScriptureSourceBrowser'
+import LibraryBookBrowser from '../components/LibraryBookBrowser'
 import { hasSearchMatch } from '../utils/searchHighlight'
 import {
   getAngTargets,
@@ -2054,11 +2055,7 @@ export default function Banis() {
               </div>
 
               <div className="mt-4">
-                <ScriptureSourceBrowser
-                  dataTestId="read-books-browser-shared"
-                  sectionIds={['panth-prakash-english']}
-                  sectionClassName="read-source-browser-card surface-primary px-4 py-4"
-                />
+                <LibraryBookBrowser dataTestId="read-books-browser-shared" />
               </div>
             </section>
           </div>
